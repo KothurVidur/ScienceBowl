528f419d30b5fc16d9b00cbb4a8fe48a0de4d028
KothurVidur <vidur@VidurHP.localdomain>
2026-03-08 22:17:00 -0400

    Updated font issues

--- backend/src/server.js
+++ backend/src/server.js
@@ -79,8 +79,9 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
       scriptSrc: ["'self'"],
+      fontSrc: ["'self'", 'data:', 'https://fonts.gstatic.com'],
       imgSrc: ["'self'", 'data:', 'blob:']
     }
   },
